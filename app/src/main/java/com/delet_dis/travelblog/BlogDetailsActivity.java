b434a0a1cc4b189fc8cd119435a6b6bd8994b4e0
delet-dis <efimov.036@gmail.com>
2021-02-13 00:04:59 +0700

    changed fonts, changed blog details activity

--- app/src/main/java/com/delet_dis/travelblog/BlogDetailsActivity.java
+++ app/src/main/java/com/delet_dis/travelblog/BlogDetailsActivity.java
@@ -2,8 +2,12 @@ package com.delet_dis.travelblog;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
@@ -39,6 +43,11 @@ public class BlogDetailsActivity extends AppCompatActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_blog_details);
+
+	Window window = getWindow();
+	window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+	window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
 	imageMain = findViewById(R.id.imageMain);
 	imageAvatar = findViewById(R.id.imageAvatar);
